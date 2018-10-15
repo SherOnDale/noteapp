562ace266a82241d32d52c3c89cf3747f18563bb
davemowery <dave.mowery@gmail.com>
2018-10-15 11:32:57 -0400

    first commit

---
+++ notes-app.js
@@ -0,0 +1,41 @@
+
+const notes = [{
+    Title: 'my Next Trip',
+    Body: 'I would like to go to Spain'
+}, {
+    Title: 'Habits to work on',
+    Body: 'Exercise. Eating Better'
+}, {
+    Title: 'Office Modifications',
+    Body: 'Get a new seat'
+}]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.Title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    document.querySelector('#notes').innerHTML = ''
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.Title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+document.querySelector('#filter-by').addEventListener('change', function(e){
+    
+})
+renderNotes(notes, filters)
+document.querySelector('#create-note').addEventListener("click", function(e){
+    e.target.textContent='You pushed it!'
+})
+
+
+document.querySelector('#search-text').addEventListener('input', function(e){
+   filters.searchText=e.target.value
+   renderNotes(notes, filters)
+})
+
